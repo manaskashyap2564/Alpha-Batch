e2b83170ccbccd3c3517ced70d7aea9a855f0716
manaskashyap2564 <kashyap.manas.glams@gmail.com>
2025-05-28 23:04:59 +0530

    arraysP2

--- arrays/noOfSubArrays.java
+++ arrays/noOfSubArrays.java
@@ -1,10 +1,22 @@
 package arrays;
 
 public class noOfSubArrays {
-    public static void printSubArrays(){
-        
+    public static void printSubArrays(int arr[]){
+        int ts = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                for (int k = i; k <=j; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                ts++;
+                System.out.println();
+            }
+            System.out.println("-----------------");
+        }       
+        System.out.println("Total No. of subarrys is : " + ts);
     }
     public static void main(String[] args) {
-        
+        int arr[] = {1,3,5,6,7,8};
+        printSubArrays(arr);
     }
 }
