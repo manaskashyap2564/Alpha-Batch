612b53f5b924233d0f0e929347268cf350db0736
manaskashyap2564 <kashyap.manas.glams@gmail.com>
2025-06-06 23:56:25 +0530

    Index Updation D9

--- arrays/arrayQuestions.java
+++ arrays/arrayQuestions.java
@@ -1,7 +1,11 @@
 package arrays;
 
 public class arrayQuestions {
-    
+    // Question 1
+    // Question 2
+    // Question 3
+    // Question 4
+    // Question 5
     public static void main(String[] args) {
         
     }
