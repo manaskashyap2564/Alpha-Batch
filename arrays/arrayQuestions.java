09f6b58b357970b691fd37df0f0f7bfa7e1e61d0
manaskashyap2564 <kashyap.manas.glams@gmail.com>
2025-06-07 23:44:10 +0530

    Initial commit for D10

--- arrays/arrayQuestions.java
+++ arrays/arrayQuestions.java
@@ -1,11 +1,17 @@
 package arrays;
 
 public class arrayQuestions {
+
     // Question 1
+
     // Question 2
+
     // Question 3
+
     // Question 4
+
     // Question 5
+    
     public static void main(String[] args) {
         
     }
