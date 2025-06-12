4ecda04e224b50fdd2df73d80a587aae6e6034cc
manaskashyap2564 <kashyap.manas.glams@gmail.com>
2025-06-12 23:56:05 +0530

    jusst updating the structure :)

--- arrays/arrayQuestions.java
+++ arrays/arrayQuestions.java
@@ -4,19 +4,13 @@ public class arrayQuestions {
 
     // Question 1
 
-
     // Question 2
-
 
     // Question 3
 
-
-
     // Question 4
 
-
     // Question 5
-    
     
     public static void main(String[] args) {
         
